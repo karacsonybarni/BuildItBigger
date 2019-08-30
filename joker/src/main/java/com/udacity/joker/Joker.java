87ca4abc3fd503861ca9f948d60629972568ae78
barnakaracsony <git8.XyZ>
2019-08-30 15:47:08 +0200

    java library implemented and used in the app

---
+++ joker/src/main/java/com/udacity/joker/Joker.java
@@ -0,0 +1,11 @@
+package com.udacity.joker;
+
+public class Joker {
+
+    private static final String DEFAULT_JOKE =
+            "Why don't blind people skydive?\nBecause it scares the crap out of their dogs.";
+
+    public String getJoke() {
+        return DEFAULT_JOKE;
+    }
+}
